66159f7947fc992860b092b58c718342154bd8e4
Faith <faith@Faithde-MacBook-Pro.local>
2025-09-30 00:10:38 +0800

    修正cors設定

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import axios from 'axios'; // 新增這行
+import axios from 'axios';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
 
